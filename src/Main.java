3e5766e32d4d94127aedf2bfe1595cfbe9a8c25e
Kutman <kutman.usupov@gmail.com>
2022-10-18 12:50:50 +0600

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,11 @@
 import models.Airplane;
 import models.Car;
+import models.Runner;
 import models.Train;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("0185KG",5,60);
-        Train train = new Train("5972",60,80);
-        Airplane airplane = new Airplane("AirEarth",100,250);
-        car.transportNumber();
-        car.seat();
-        car.ride();
-        car.newWheel();
-        train.transportNumber();
-        train.seat();
-        train.ride();
-        airplane.transportNumber();
-        airplane.seat();
-        airplane.ride();
+        Runner runner = new Runner();
+        runner.console();
     }
 }
